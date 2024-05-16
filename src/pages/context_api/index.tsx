253cd8a7174a11f81994d6eb92034b5816d0a7c0
MagnoLevi <magnolevi2003@gmail.com>
2024-05-15 21:29:19 -0300

    Commit

--- src/pages/context_api/index.tsx
+++ src/pages/context_api/index.tsx
@@ -1,7 +1,17 @@
+import { FaMoon, FaSun } from "react-icons/fa";
+import "./style.css"
+
 const ContextApi = () => {
-    return(
-        <div>
-            
+    let darkTheme = false;
+
+    return (
+        <div className="theme-container">
+            <span className="theme-icon">
+                {darkTheme ? <FaMoon /> : <FaSun />}
+            </span>
+            <button className="theme-button">
+                <h1>Enable {darkTheme ? "light" : "dark"} theme </h1>
+            </button>
         </div>
     )
 }
